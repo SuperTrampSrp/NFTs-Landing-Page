701f4023c2cd0fc0aa2a5b78817fbf13d65895c0
SuperTrampSrp <65632897+SuperTrampSrp@users.noreply.github.com>
2024-10-17 19:01:08 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -2,99 +2,70 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="text-slate-50 flex flex-col justify-between h-screen">
+      <nav className="flex p-5 items-center pl-28 ">
+        <div className="w-5/12 animate-jump-in animate-once animate-delay-[2800ms] animate-ease-in">
+          <Image
+            width={80}
+            height={50}
+            src='/logo.png'
+          />
+        </div>
+        <div className="w-7/12 flex justify-around animate-fade-down animate-delay-[1800ms] animate-ease-in">
+          <ul className="flex justify-between gap-5 font-bold items-center">
+            <li className="hover:text-red-600">Top Artists</li>
+            <li className="hover:text-red-600">Marketpalce</li>
+            <li className="hover:text-red-600">Collection</li>
+            <li className="hover:text-red-600">About Us</li>
+          </ul>
+          <button className="bg-gradient-to-r from-red-600 to-red-300 font-bold px-5 py-2 border border-none rounded-sm">Get Started</button>
+        </div>
+      </nav>
+      <main className="flex px-28">
+        <div className="flex flex-col w-6/12 animate-fade-right animate-ease-in">
+          <div>
+            <h1 className="font-bold text-7xl">Discover, <span className="bg-gradient-to-r from-red-600 to-red-300 text-transparent bg-clip-text"> Collect</span>, and sell <span className="bg-gradient-to-r from-red-600 to-red-300 text-transparent bg-clip-text">Extraordinary</span>  NFTs</h1>
+            <p className="font-bold mt-10 text-xl">A marketplace  that has popular  artist from all  around </p>
+          </div>
+          <div className="flex gap-5 mt-10">
+            <button className="bg-gradient-to-r from-red-600 to-red-300 font-bold text-slate-50 px-5 py-2 border border-red-600 rounded-sm">Explore Now</button>
+            <button className="px-5 text-red-600 border-2 border-red-600 rounded-sm">Collect Wallet</button>
+          </div>
+        </div>
+        <div className="relative flex ">
+          <Image
+            width={280}
+            height={50}
+            src='/hero.png'
+            className="bg-transparent animate-jump-in animate-delay-700 animate-ease-in-out animate-duration-500"
+          />
+          <div className="flex h-20 bg-slate-50 p-5 absolute gap-10 justify-around bg-opacity-20 top-96 left-28 border border-none rounded-lg text-white animate-flip-down animate-delay-1000 animate-ease-in">
+            <div className="flex flex-col min-w-28 justify-center items-center font-bold">
+              <span className="text-slate-400">Current Bid</span>
+              <span>$5.56</span>
+            </div>
+            <div className="flex flex-col min-w-28 justify-center items-center font-bold">
+              <span className="text-slate-400">Upcoming Bid</span>
+              <span>$7.26</span>
+            </div>
+            <button className="bg-gradient-to-r from-red-600 to-red-300 font-bold text-slate-50 px-5 py-2 border border-red-600 rounded-sm w-28">Bid Now</button>
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer className="px-28 items-end bottom-0 float-end py-5 flex gap-10 pb-10 animate-fade-right animate-delay-[1500ms] animate-ease-in">
+        <div className="flex flex-col gap-2">
+          <span className="text-xl font-bold">1526K</span>
+          <p>Users</p>
+        </div>
+        <div className="flex flex-col gap-2 border-l-4 pl-7">
+          <span className="text-xl font-bold">326K</span>
+          <p>Artworks</p>
+        </div>
+        <div className="flex flex-col gap-2 border-l-4 pl-7">
+          <span className="text-xl font-bold">316K</span>
+          <p>Artists</p>
+        </div>
       </footer>
     </div>
   );
